daa6053f9455e2a56714408590e4c58a90ee5664
Stefan Wiedemann <wistefan@googlemail.com>
2021-03-01 16:00:06 +0100

    support range and list queries

--- src/main/java/org/fiware/mintaka/domain/query/ComparisonTerm.java
+++ src/main/java/org/fiware/mintaka/domain/query/ComparisonTerm.java
@@ -10,10 +10,7 @@ import java.text.NumberFormat;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.time.LocalDateTime;
-import java.util.Date;
-import java.util.List;
-import java.util.Objects;
-import java.util.Optional;
+import java.util.*;
 
 import static org.fiware.mintaka.rest.TemporalApiController.WELL_KNOWN_ATTRIBUTES;
 
@@ -60,11 +57,15 @@ public class ComparisonTerm extends QueryTerm {
 		if (isCompound()) {
 			// query inside a compound
 		}
-		if (isList()) {
+		Optional<String> optionalListQuery = getListQuery();
+ 		if (optionalListQuery.isPresent()) {
 			// query for list
+			return optionalListQuery.get();
 		}
-		if (isRange()) {
+		Optional<String> optionalRangeQuery = getRangeQuery();
+		if (optionalRangeQuery.isPresent()) {
 			// query for range
+			return optionalRangeQuery.get();
 		}
 		Optional<String> optionalStringValue = getStringValue();
 		if (optionalStringValue.isPresent()) {
@@ -143,53 +144,118 @@ public class ComparisonTerm extends QueryTerm {
 		return pathToCheck.contains(LEFT_SQUARE_BRACKET_STRING) && pathToCheck.contains(RIGHT_SQUARE_BRACKET_STRING);
 	}
 
-	private boolean isRange() {
-		return getStringValue().isEmpty() && comparisonValue.contains(RANGE_SEPERATOR);
+	private Optional<String> getRangeQuery() {
+		IllegalArgumentException exception = new IllegalArgumentException(String.format("%s is not a valid range.", comparisonValue));
+		if (!getStringValue().isEmpty() || !comparisonValue.contains(RANGE_SEPERATOR)) {
+			return Optional.empty();
+		}
+		String[] rangeValue = comparisonValue.split(RANGE_SEPERATOR);
+		if (rangeValue.length != 2) {
+			throw exception;
+		}
+
+		// kind of code duplication is required to ensure early exit.
+
+		Optional<String> optionalStringValueMin = getStringValue(rangeValue[0]);
+		Optional<String> optionalStringValueMax = getStringValue(rangeValue[1]);
+		if (optionalStringValueMin.isPresent() && optionalStringValueMax.isPresent()) {
+			// query for string
+			return Optional.of(String.format(" text BETWEEN '%s' AND '%s'", optionalStringValueMin.get(), optionalStringValueMax.get()));
+		}
+		Optional<Number> optionalNumberValueMin = getNumberValue(rangeValue[0]);
+		Optional<Number> optionalNumberValueMax = getNumberValue(rangeValue[1]);
+		if (optionalNumberValueMin.isPresent() && optionalNumberValueMax.isPresent()) {
+			// query for number
+			return Optional.of(String.format(" number BETWEEN %s AND %s", optionalNumberValueMin.get(), optionalNumberValueMax.get()));
+		}
+		Optional<String> optionalTimeValueMin = getTimeValue(rangeValue[0]);
+		Optional<String> optionalTimeValueMax = getTimeValue(rangeValue[0]);
+		if (optionalTimeValueMin.isPresent() && optionalTimeValueMax.isPresent()) {
+			// query for time
+			return Optional.of(String.format(" datetime::time BETWEEN %s::time AND %s::time", optionalTimeValueMin.get(), optionalTimeValueMax.get()));
+		}
+		Optional<String> optionalDateValueMin = getDateValue(rangeValue[0]);
+		Optional<String> optionalDateValueMax = getDateValue(rangeValue[1]);
+		if (optionalDateValueMin.isPresent() && optionalDateValueMax.isPresent()) {
+			// query for date
+			return Optional.of(String.format(" datetime BETWEEN %s AND %s", optionalDateValueMin.get(), optionalDateValueMax.get()));
+		}
+		Optional<String> optionalDateTimeValueMin = getDateTimeValue(rangeValue[0]);
+		Optional<String> optionalDateTimeValueMax = getDateTimeValue(rangeValue[0]);
+		if (optionalDateTimeValueMin.isPresent() && optionalDateTimeValueMax.isPresent()) {
+			// query for datetime
+			return Optional.of(String.format(" datetime BETWEEN %s AND %s", optionalDateTimeValueMin.get(), optionalDateTimeValueMax.get()));
+		}
+		throw exception;
 	}
 
-	private boolean isList() {
-		return comparisonValue.charAt(0) == QueryParser.OPEN_BRACE && comparisonValue.charAt(0) == QueryParser.CLOSE_BRACE;
+	private Optional<String> getListQuery() {
+		boolean isList = comparisonValue.charAt(0) == QueryParser.OPEN_BRACE && comparisonValue.charAt(comparisonValue.length() - 1) == QueryParser.CLOSE_BRACE;
+		if (!isList) {
+			return Optional.empty();
+		}
+		return Optional.of(
+				"boolean IN " + comparisonValue + " " +
+						"OR text IN " + comparisonValue + " " +
+						"OR number IN " + comparisonValue + " " +
+						"OR datetime IN " + comparisonValue);
 	}
 
-	private Optional<String> getStringValue() {
-		if (comparisonValue.charAt(0) == QueryParser.QUOTE && comparisonValue.charAt(comparisonValue.length() - 1) == QueryParser.QUOTE) {
+	private Optional<String> getStringValue(String toCheck) {
+		if (toCheck.charAt(0) == QueryParser.QUOTE && toCheck.charAt(toCheck.length() - 1) == QueryParser.QUOTE) {
 			// return everything except the quotes
-			return Optional.of(comparisonValue.substring(1, comparisonValue.length() - 1));
+			return Optional.of(toCheck.substring(1, toCheck.length() - 1));
 		}
 		return Optional.empty();
 	}
 
-	private Optional<Number> getNumberValue() {
+	private Optional<String> getStringValue() {
+		return getStringValue(comparisonValue);
+	}
+
+	private Optional<Number> getNumberValue(String toCheck) {
 		try {
-			return Optional.of(NumberFormat.getInstance().parse(comparisonValue));
+			return Optional.of(NumberFormat.getInstance().parse(toCheck));
+		} catch (ParseException e) {
+			return Optional.empty();
+		}
+	}
+
+	private Optional<Number> getNumberValue() {
+		return getNumberValue(comparisonValue);
+	}
+
+	private Optional<String> getTimeValue(String toCheck) {
+		try {
+			TIME_FORMAT.parse(toCheck);
+			// postgres supports the same time format as ngsi-ld, except that it uses . for ms separation instead of ,
+			return Optional.of(toCheck.replace(',', '.'));
 		} catch (ParseException e) {
 			return Optional.empty();
 		}
 	}
 
 	private Optional<String> getTimeValue() {
+		return getTimeValue(comparisonValue);
+	}
+
+	private Optional<String> getDateValue(String toCheck) {
 		try {
-			TIME_FORMAT.parse(comparisonValue);
-			// postgres supports the same time format as ngsi-ld, except that it uses . for ms separation instead of ,
-			return Optional.of(comparisonValue.replace(',', '.'));
+			DATE_FORMAT.parse(toCheck);
+			return Optional.of(toCheck);
 		} catch (ParseException e) {
 			return Optional.empty();
 		}
 	}
 
 	private Optional<String> getDateValue() {
-		try {
-			DATE_FORMAT.parse(comparisonValue);
-			return Optional.of(comparisonValue);
-		} catch (ParseException e) {
-			return Optional.empty();
-		}
+		return getDateValue(comparisonValue);
 	}
 
-	private Optional<String> getDateTimeValue() {
+	private Optional<String> getDateTimeValue(String toCheck) {
 		try {
-			if (LocalDateTime.parse(comparisonValue) != null) {
-				return Optional.of(comparisonValue.replace(',', '.'));
+			if (LocalDateTime.parse(toCheck) != null) {
+				return Optional.of(toCheck.replace(',', '.'));
 			}
 			return Optional.empty();
 		} catch (RuntimeException e) {
@@ -197,13 +263,22 @@ public class ComparisonTerm extends QueryTerm {
 		}
 	}
 
-	private Optional<Boolean> getBooleanValue() {
-		if (comparisonValue.equals("true")) {
+	private Optional<String> getDateTimeValue() {
+		return getDateTimeValue(comparisonValue);
+	}
+
+	private Optional<Boolean> getBooleanValue(String toCheck) {
+		if (toCheck.equals("true")) {
 			return Optional.of(true);
 		}
-		if (comparisonValue.equals("false")) {
+		if (toCheck.equals("false")) {
 			return Optional.of(false);
 		}
 		return Optional.empty();
+	}
+
+	private Optional<Boolean> getBooleanValue() {
+		return getBooleanValue(comparisonValue);
+
 	}
 }
