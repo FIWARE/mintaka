b8fc4e13018b64e345bd1f16cbf019d081b4d694
Tim Smyth <tim.smyth@fiware.org>
2023-11-28 10:53:47 +0100

    logs

--- src/test/java/org/fiware/mintaka/domain/query/ngsi/ComparisonTermTest.java
+++ src/test/java/org/fiware/mintaka/domain/query/ngsi/ComparisonTermTest.java
@@ -1,14 +1,9 @@
 package org.fiware.mintaka.domain.query.ngsi;
 
-import io.micronaut.test.extensions.junit5.annotation.MicronautTest;
-import jakarta.inject.Inject;
-import org.fiware.mintaka.QueryingTest;
 import org.fiware.mintaka.context.ContextProperties;
 import org.fiware.mintaka.context.LdContextCache;
-import org.fiware.mintaka.domain.query.temporal.TimeRelation;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
@@ -17,7 +12,6 @@ import java.net.URL;
 import java.text.NumberFormat;
 import java.text.ParseException;
 import java.util.List;
-import java.util.Optional;
 import java.util.stream.Stream;
 
 class ComparisonTermTest {
@@ -30,24 +24,31 @@ class ComparisonTermTest {
 		ldContextCache.initDefaultContext();
 		queryParser = new QueryParser(ldContextCache);
 	}
+
 	private static Stream<Arguments> getQueries() {
 		return Stream.of(
 				Arguments.of("motor.fuel==0.6..0.8", " instanceid in (SELECT attrinstanceid  FROM subattributes sa WHERE  number BETWEEN 0.6 AND 0.8 and id='https://uri.etsi.org/ngsi-ld/default-context/fuel')"),
 				Arguments.of("motor.fuel==0,6..0,8", " instanceid in (SELECT attrinstanceid  FROM subattributes sa WHERE  number BETWEEN 0.6 AND 0.8 and id='https://uri.etsi.org/ngsi-ld/default-context/fuel')")
 		);
 	}
+
 	@ParameterizedTest
 	@MethodSource("getQueries")
 	void toSQLQuery(String rawQuery, String expectedResult) throws Exception {
 		QueryTerm query = queryParser.toTerm(rawQuery, List.of(new URL("https://uri.etsi.org/ngsi-ld/v1/ngsi-ld-core-context.jsonld")));
-		Assertions.assertEquals(expectedResult,query.toSQLQuery());
+		String sqlQuery = query.toSQLQuery();
+		System.out.println(rawQuery + " -> " +sqlQuery);
+		Assertions.assertEquals(expectedResult, query.toSQLQuery());
 	}
 
 	@ParameterizedTest
 	@MethodSource("getNumbers")
 	void parseNumber(String input, Number expected) throws ParseException {
-		Assertions.assertEquals(expected,NumberFormat.getInstance().parse(input));
+		Number parsedNumber = NumberFormat.getInstance().parse(input);
+		System.out.println(input + " -> " +parsedNumber);
+		Assertions.assertEquals(expected, parsedNumber);
 	}
+
 	private static Stream<Arguments> getNumbers() {
 		return Stream.of(
 				Arguments.of("0.6", 0.6),
